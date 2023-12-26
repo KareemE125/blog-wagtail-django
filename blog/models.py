955bb7e525493fb35c14ba806d9a92fb2a9f9271
Kareem Ezzat <Kareemezzat.125@gmail.com>
2023-12-26 16:42:34 +0200

    Blog and posts

--- blog/models.py
+++ blog/models.py
@@ -3,11 +3,34 @@ from django.db import models
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel
+from wagtail.search import index
 # Create your models here.
 
-class BolgPage(Page):
+class BlogPage(Page):
     body = RichTextField(blank=True)
     
     content_panels = Page.content_panels + [
-        FieldPanel('body', classname="full")
+        FieldPanel('body')
+    ]
+    
+    def get_context(self, request):
+        context = super().get_context(request)
+        posts = self.get_children().live().order_by('-first_published_at')
+        context["posts"] = posts
+        return context
+   
+    
+class PostPage(Page):
+    date = models.DateField("Post date", auto_now_add=True)
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+    
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+    
+    content_panels = Page.content_panels + [
+        FieldPanel('body'),
+        FieldPanel('intro'),
     ]
